b3062fe2e4af2ba69077fe7dbfd1b8beb47623c9
Daniele Lombardo <33962842+danielelombardo@users.noreply.github.com>
2026-06-14 17:56:19 +0200

    knicki build

---
+++ config/hardware_layouts/knicki.h
@@ -0,0 +1,23 @@
+/*          KEY MATRIX / LAYOUT MAPPING
+
+  ╭────────────────────┬────────────────────╮ 
+  │  0   1   2   3   4 │  5   6   7   8   9 │ 
+  │ 10  11  12  13  14 │ 15  16  17  18  19 │ 
+  │ 20  21  22  23  24 │ 25  26  27  28  29 │ 
+  │ 30  31  32  33  34 │ 35  36  37  38  39 │ 
+  ╰────────────────────┴────────────────────╯ */
+
+#pragma once
+#define LAYER_FROM40( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39  \
+) \
+    k00  k01  k02  k03  k04  k05  k06  k07  k08  k09  \
+    k10  k11  k12  k13  k14  k15  k16  k17  k18  k19  \
+    k20  k21  k22  k23  k24  k25  k26  k27  k28  k29  \
+    k30  k31       k33  k34       k36  k37  k38  k39
+/*
+                        xxxx       &tog MU  &lsm NA LSHFT               &lkp MA SPACE  &mo FN   xxxx
+&mkp MB4    &mkp MB5    xxxx       &tog MU  &lsm NA LSHFT  &key_repeat  &lkp MA SPACE  &mo FN   &vold       &volu */
